200cd0ba1a837318414f0efaa5c44cc4321fe94a
MukulDabare <106649658+MukulDabare@users.noreply.github.com>
2023-08-17 21:18:46 +0530

    Project With Kapka

---
+++ practices_Consumer1/src/main/java/com/main/PracticesConsumer1Application.java
@@ -0,0 +1,25 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@EnableEurekaClient
+@SpringBootApplication
+public class PracticesConsumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticesConsumer1Application.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate rt()
+	{
+		RestTemplate rs= new RestTemplate();
+		return rs;
+	}
+
+}
